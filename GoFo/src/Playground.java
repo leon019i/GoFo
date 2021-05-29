623fb40417986e9186bbbf26d348b727abb714e6
loay <leonlord0@gmail.com>
2021-05-29 22:11:49 +0200

    loay_task

---
+++ GoFo/src/Playground.java
@@ -0,0 +1,18 @@
+public class Playground {
+    String name;
+    int freeSlots;
+    boolean state;
+    double cost;
+    String location;
+    float size;
+
+
+    public Playground(String name, int freeSlots, boolean state, double cost, String location, Float size) {
+        this.name = "name";
+        this.freeSlots = 0 ;
+        this.state =false;
+        this.cost = 0;
+        this.location = "";
+        this.size = 0;
+    }
+}
--- GoFo/src/PlaygroundOwner.java
+++ GoFo/src/PlaygroundOwner.java
@@ -1,2 +1,14 @@
-public class PlaygroundOwner {
+import java.util.ArrayList;
+
+public class PlaygroundOwner extends user{
+    ArrayList<Playground> playgroundArrayList=new ArrayList<Playground>();
+    public PlaygroundOwner(String name, String location, String password, String phone, String email) {
+        super(name, location, password, phone, email);
+    }
+
+    public PlaygroundOwner() {
+    }
+    void registerPlayground(Playground playground){
+        playgroundArrayList.add(playground);
+    }
 }
