6e0aa9f51ba4935d750b9e5f80237da970f7436f
nadamousa111 <71728548+nadamousa111@users.noreply.github.com>
2021-06-04 16:04:28 +0200

    Nada_Task

--- GoFo/src/Wallet.java
+++ GoFo/src/Wallet.java
@@ -1,18 +1,42 @@
-public class Wallet {
-    private double money;
 
+/**
+ * Class Wallet represents the balance in each user of the system's e-Wallet
+ * 
+ * @author 
+ * 
+ */
+    public class Wallet {
+    private double money;
+    
+     /**
+      * 
+      * @param money money on e-Wallet of the user
+      */
     public Wallet(double money) {
         this.money = money;
     }
 
+    /**
+     * This default constructor initialises money on e-Wallet to zero 
+     */  
     public Wallet() {
         money=0;
     }
-
+ 
+   /**
+     * 
+     * @return returns the user's money on e-Wallet
+     */  
     public double getMoney() {
         return money;
     }
 
+     
+     /**
+     * This function the money of the user on e-Wallet
+     * 
+     * @param money money of the user on e-Wallet
+     */   
     public void setMoney(double money) {
         if (money<0){
         this.money = money;}
