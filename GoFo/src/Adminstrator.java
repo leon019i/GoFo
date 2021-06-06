8b070e988ea9dcb401135d9e2719e882d96cb160
nadamousa111 <71728548+nadamousa111@users.noreply.github.com>
2021-06-06 14:18:34 +0200

    Nada_Task

--- GoFo/src/Adminstrator.java
+++ GoFo/src/Adminstrator.java
@@ -1,38 +1,77 @@
 
 import java.util.ArrayList;
+import java.util.WeakHashMap;
 
-
+/**
+ * This class represents all the functionalities that the administrator does in the system such as : activating a playground , deleting a playground,etc 
+ * 
+ * @author Nada Mousa Abdelaziz
+ */
 public class Adminstrator extends user {
-     ArrayList<Playground> playgroundArrayList=new ArrayList<Playground>();
-    private Wallet wallet;
-
-    public Adminstrator(ArrayList<Playground> playgroundArrayList, Wallet wallet) {
-        this.playgroundArrayList = playgroundArrayList;
-        this.wallet = wallet;
+    ArrayList<Playground> playgroundArrayList=new ArrayList<Playground>();
+    private Wallet wallet; 
+    
+    /**
+     * 
+     * @param name name of the administrator 
+     * @param location location of the administrator 
+     * @param password password of the administrator 
+     * @param phone phone number of the administrator 
+     * @param email email of the administrator 
+     */
+    public Adminstrator(String name,String location , String password ,String phone,String email ){
+      super( name, location , password , phone, email);  
     }
-
+    
+    /**
+     * this function activate the playground and change its state from suspended to active 
+     * @param playground the playground that needs to be activated 
+     */
     public void activatePlayground(Playground playground){
-       Playground playground1 = null ;
+      // Playground playground1 = null ;
        boolean suspended = true , activated = false ; 
-       suspended = playground1.getState();
-       if(this.getBookings()!=2)
-          activated = playground1.getState();
+       suspended = playground.getState();
+        
+      for(int i=0;i< playground.Hours.size();i++)
+           if(playground.Hours.get(i).equals(playground.Hours.get(i+1)))
+        
+          activated = playground.getState();
           
+
+    }
+    
+    /**
+     * this function returns if the playground is suspended or not , if it is suspended it returns true , otherwise it is false
+     * 
+     * @param state state of the playground 
+     * @return the state of the playground , so if it is true then it is suspended otherwise it is active 
+     */
+    public boolean suspendPlayground(boolean state){
+       
+        return state; 
         
     }
     
-    public boolean suspendPlayground(boolean state){
-        Playground playground1 = null ; 
-        boolean active = true ; 
-        active = playground1.getState() ;
-       if( this.getBookings() ==2)
-           return !active ; 
-       else 
-           return active ; 
-    }
-    
+    /**
+     * this function deletes a playground from the system 
+     * @param playground playground that needs to be deleted 
+     */
     public void deletePlayground(Playground playground){
       playgroundArrayList.remove(playground);
     }
-
+    
+    /**
+     * This function displays the balance of both the player and the playground owner of this playground 
+     * 
+     * @param player player in the system 
+     * @param playgroundOwner the playground owner that owns the playground that this player booked 
+     */
+   public void viewAllWallets(Player player, PlaygroundOwner playgroundOwner){
+        
+     player.checkMoney();
+     playgroundOwner.checkMoney();
+       
+     
+    }
+    
 }
